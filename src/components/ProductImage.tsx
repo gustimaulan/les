8d0af6f564b8d92fda55c5f175d3f72f1dde3b69
G Dhia <gmaulan47@gmail.com>
2024-08-11 16:50:37 +0700

    delete placeholder

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react"
-
 function ProductImage() {
-  const [isloaded, setIsLoaded] = useState(false)
   return (
     <section id="product-images">
-      <div className="flex items-center justify-center size-[640px] text-center mx-auto bg-gray-400">
-        <div 
-        className={`text-2xl txt-gray-300 w-full + ${isloaded ? "hidden" : "block" }`}></div>
+      <div className="container max-w-screen-sm text-center mx-auto bg-gray-400">
         <img src="images/les-privat-1.webp" alt="les privat" 
-        className={`w-full + ${isloaded ? "opacity-100" : "opacity-0"}`} 
-        onLoad={() => {setIsLoaded(true)}} />
+        className="w-full" 
+         />
       </div>
     </section>
   )
