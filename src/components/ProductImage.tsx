8295ae8fd6385a5c313736b569d9ff76d015b004
G Dhia <gmaulan47@gmail.com>
2024-08-11 16:15:59 +0700

    edit placeholder again

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,27 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 function ProductImage() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
+  const [isloaded, setIsLoaded] = useState(false)
   return (
     <section id="product-images">
-      <div className="max-w-screen-sm mx-auto relative">
-        {/* Placeholder image from CDN */}
-        <img
-          src="https://via.placeholder.com/640x640?text=Loading+Image..." 
-          alt="loading placeholder"
-          className={`w-full h-auto object-cover transition-opacity duration-500 ${isLoaded ? 'opacity-0' : 'opacity-100'}`}
-        />
-        {/* Real image */}
-        <img
-          src="images/les-privat-1.webp"
-          alt="les privat"
-          className={`w-full h-auto object-cover absolute top-0 left-0 transition-opacity duration-500 ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
-          onLoad={() => setIsLoaded(true)}
-        />
+      <div className="flex items-center justify-center size-[640px] text-center mx-auto bg-gray-400">
+        <div 
+        className={`text-2xl txt-gray-300 w-full + ${isloaded ? "hidden" : "block" }`}></div>
+        <img src="images/les-privat-1.webp" alt="les privat" 
+        className={`w-full + ${isloaded ? "opacity-100" : "opacity-0"}`} 
+        onLoad={() => {setIsLoaded(true)}} />
       </div>
     </section>
-  );
+  )
 }
 
-export default ProductImage;
+export default ProductImage
