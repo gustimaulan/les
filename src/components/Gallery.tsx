3855607e957ea2a44528784949bc5df9b5cc0800
G Dhia <gmaulan47@gmail.com>
2024-09-17 14:12:25 +0700

    edit image size

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -9,7 +9,7 @@ import { GALLERYIMAGES, ImagesProps } from "../utils/GALLERYIMAGES"
       <div className="grid grid-cols-2 gap-2">
       {GALLERYIMAGES.map((image, index) => (
         <div key={index} className="p-2">
-          <img src={image.url} alt={image.alt} className="rounded-t-lg h-64 w-full object-cover object-center" />
+          <img src={image.url} alt={image.alt} className="rounded-t-lg h-52 w-full object-cover object-center" />
           <p className="text-center text-xs bg-gray-100 p-2 rounded-b-lg">{image.caption}</p>
         </div>
       ))}
